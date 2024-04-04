038449e804e244974738a41a8092ef4816b6723b
yasserelhor <elhoryasser6@gmail.com>
2024-04-04 10:30:31 +0000

    adding the thing

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,49 +1,32 @@
 #!/usr/bin/python3
 """
-Utilizes the do_deploy function to distribute an archive to my web servers.
+This Fabric Script distributes an archive to your web servers,
+using the function do_deploy
 """
-from fabric.api import *
+from fabric.api import local, put, run, env, sudo
+from os import path
 from datetime import datetime
-import os
 
-env.hosts = ['54.144.136.64', '100.24.236.179']
-env.user = 'ubuntu'
+env.hosts = ['34.227.101.152', '54.160.126.125']
 
-
-def do_pack():
-    """
-    Creates a .tgz archive from the contents within the web_static folder.
-    """
-    try:
-        local('mkdir -p versions')
-        datetime_format = '%Y%m%d%H%M%S'
-        archive_path = 'versions/web_static_{}.tgz'.format(
-            datetime.now().strftime(datetime_format))
-        local('tar -cvzf {} web_static'.format(archive_path))
-        print('web_static packed: {} -> {}'.format(archive_path,
-              os.path.getsize(archive_path)))
-    except:
-        return None
 
 def do_deploy(archive_path):
-    """
-    Deploys the archive to the web server.
-    """
-    if not os.path.exists(archive_path):
+    """Deploy Function"""
+    if not path.exists(archive_path):
         return False
-    file_name = archive_path.split('/')[1]
-    file_path = '/data/web_static/releases/'
-    releases_path = file_path + file_name[:-4]
     try:
-        put(archive_path, '/tmp/')
-        run('mkdir -p {}'.format(releases_path))
-        run('tar -xzf /tmp/{} -C {}'.format(file_name, releases_path))
-        run('rm /tmp/{}'.format(file_name))
-        run('mv {}/web_static/* {}/'.format(releases_path, releases_path))
-        run('rm -rf {}/web_static'.format(releases_path))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {} /data/web_static/current'.format(releases_path))
-        print('New version deployed!')
+        put(archive_path, "/tmp/")
+        name = archive_path.split('/')[1].split('.')[0]
+        sudo("mkdir -p /data/web_static/releases/{}/".format(name))
+        sudo("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/"
+             .format(name, name))
+        run("rm /tmp/{}.tgz".format(name))
+        sudo("mv /data/web_static/releases/{}/web_static/*\
+             /data/web_static/releases/{}/".format(name, name))
+        sudo("rm -rf /data/web_static/releases/{}/web_static".format(name))
+        sudo("rm -rf /data/web_static/current")
+        sudo("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+             .format(name))
         return True
-    except:
-        return False
+    except Exception:
+        return 
